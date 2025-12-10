2c79062c19865e42b4c479bf09f3042aa74fa9dd
David <davsermel@gmail.com>
2025-12-10 13:52:11 +0100

    poder accedir a  usuaris

--- bo/Productes/js/AtributsModificar.js
+++ bo/Productes/js/AtributsModificar.js
@@ -147,6 +147,6 @@ async function guardarEnLocalStorage(e) {
     };
 
        await updateId(url, "Attribute", atributoId,datosobjecte);
-    alert("Característica modificada correctament!");
+ 
     window.location.href = "./Atributs.html";
 }
